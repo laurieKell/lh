bcf72ccfc62f2f2a2ff55db31a473439b3f173de
laurie <laurie@kell.es>
2014-10-08 09:04:12 +0200

    von B

--- R/lh-growth-vonB.R
+++ R/lh-growth-vonB.R
@@ -20,11 +20,23 @@
 #' }
 vonB=function(par,age){
   res=par["linf"]%*%(1.0-exp((-par["k"])%*%(age%-%par["t0"])))
+  
   dimnames(res)=dimnames(age)
   res}
 
-invVonB=function(par,len)
-  -(log(1.0-(len%/%par["linf"]))%/%par["k"])%+%par["t0"]
+invVonB=function(par,len){
+  res=log(1-(len%/%par["linf"]))%/% (-par["k"])%+%par["t0"]
+
+  dimnames(res)=dimnames(len)
+  res}
+
+# library(numDeriv)
+# par=FLPar(linf=318.9,k=0.093,t0=-0.970)
+  # fnL=function(len) invVonB(par,FLQuant(len))
+  # fnA=function(age)    vonB(par,FLQuant(age,dimnames=list(age=age)))
+  # 
+  # grad(fnL,fnA(15))
+
 
 
 # setGeneric('vonB', function(params,data, ...)
